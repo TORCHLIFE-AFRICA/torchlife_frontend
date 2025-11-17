01ba32c3c743de094773e39b65fe57fa7864cffc
nobleszn <temitopesamadeniyi@gmail.com>
2025-11-18 00:29:30 +0100

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import HeroSectionPage from "@/module/heroSection";
-import ButtonComponent from "@/components/buttons/src/Buttons";
 
 export default function Home() {
   return (
     <main>
       <HeroSectionPage />
-      <ButtonComponent />
-    
-    
     </main>
   );
 }
